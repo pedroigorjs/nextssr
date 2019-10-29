5acb86318605e9c216f2056a4bfa2f51bc689378
Pedro Sampaio <pedroigorjs@gmail.com>
2019-10-29 17:08:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nextssr
+A project in Next.js from course RocketSeat GoStack Bootcamp.
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Link from 'next/link'
+
+const Home = () => (
+  <div>
+    <h1>Hello World!</h1>
+    <Link href='/users'>
+      <a>Users</a>
+    </Link>
+  </div>
+)
+
+export default Home
